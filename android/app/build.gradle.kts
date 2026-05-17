c822511f43826b7e75720ca4772e6f4e3a95fc3e
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-17 12:47:59 +0530

    chore: update Android build configuration and manifest settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.jeev_sathi"
+    namespace = "com.jeevsathi.sinux.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.jeev_sathi"
+        applicationId = "com.jeevsathi.sinux.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
