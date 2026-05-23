5ecc3dc25d7cbf818b7e7727eb00a63340513204
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-23 05:58:17 +0530

    feat: update Cloudinary configuration setup

--- lib/config/cloudinary_config.dart
+++ lib/config/cloudinary_config.dart
@@ -1,5 +1,9 @@
+import '../services/env_loader.dart';
+
 class CloudinaryConfig {
-  static const String cloudName = 'dk8tnszro';
-  static const String uploadPreset = 'jeevsathi';
+  static String get cloudName => EnvLoader.get('CLOUDINARY_CLOUD_NAME', fallback: 'dk8tnszro');
+  static String get uploadPreset => EnvLoader.get('CLOUDINARY_UPLOAD_PRESET', fallback: 'jeevsathi');
+  static String get apiKey => EnvLoader.get('CLOUDINARY_API_KEY', fallback: '695158693225734');
+  static String get apiSecret => EnvLoader.get('CLOUDINARY_API_SECRET', fallback: 'Kx67kaAJ1mInY89qMudv0px4IbY');
 }
 
