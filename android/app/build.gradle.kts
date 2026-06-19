1a0261b7443decf6a513a5c84eeebc4c1a0f52ee
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-19 20:05:32 +0530

    chore(android): update Android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.jeevsathi.sinux.app"
+    namespace = "com.jeevsathi.sinux"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.jeevsathi.sinux.app"
+        applicationId = "com.jeevsathi.sinux"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
