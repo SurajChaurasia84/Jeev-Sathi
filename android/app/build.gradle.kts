7618b3db4c60bd4011ed007440e3c91393fe417e
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-20 02:26:57 +0530

    added anonyms login

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.jeevsathi.sinux"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
